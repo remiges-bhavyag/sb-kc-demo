d79ad6e12cbafc3acc216c238104c35b2765aa1d
merce-bhavyag <bhavya.gandhi@merce.co>
2023-05-25 17:35:29 +0530

    #268: added comments to code

--- src/main/java/co/merce/config/WebSecurityConfig.java
+++ src/main/java/co/merce/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package co.merce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,18 +27,46 @@ public class WebSecurityConfig {
 	
 	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests(requests -> requests
+        		/*
+                 * We can set Global Request-Role Matchers here. Following are some of the examples.
+                 */
+        		//.requestMatchers(HttpMethod.GET, "/test/admin", "/test/admin/**").hasRole("admin")
+        		//.requestMatchers(HttpMethod.GET, "/test/user").hasAnyRole("user", "admin")
+        		//.requestMatchers(HttpMethod.GET, "/test/anonymous", "/test/anonymous/**").permitAll()
+        		
+        		/*
+                 * Alternatively, these can be set directly on the methods. In case you want to
+                 * use Roles defined on methods, then you have to add @EnableMethodSecurity
+                 * annotation
+                 */
+        		.anyRequest()
+        		.authenticated()
+        		);
+        
+        /*
+         * Add JWT Auth Converter to extract the Roles from JWT
+         */
         http.oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
                 );
+        /*
+         * Set HTTP session management policy
+         */
         http.sessionManagement(sessionManagement ->
 			sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			/**
+			 * Following policies can be set if need be
+			 */
 				//.sessionConcurrency(sessionConcurrency ->
 				//	sessionConcurrency
 				//		.maximumSessions(1)
 				//		.expiredUrl("/login?expired")
 				//)
 		);
+        /*
+         * Return the HTTP object
+         */
         return http.build();
     }
 	
